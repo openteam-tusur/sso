9212fc89aaec9d22cc0f122443811cd63e3799a5
Evgeny Lapin <lev@openteam.ru>
2012-01-24 15:16:04 +0700

    "настраиваемое" обратное мыло для роботописем devis`а

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -9,5 +9,6 @@ Settings.define 'hoptoad.host',               :env_var => 'HOPTOAD_HOST'
 Settings.define 'mailer.app_host',            :env_var => 'MAILER_APP_HOST'
 Settings.define 'mailer.smtp_address',        :env_var => 'MAILER_SMTP_ADDRESS'
 Settings.define 'mailer.smtp_port',           :env_var => 'MAILER_SMTP_PORT'
+Settings.define 'mailer.sender_email',        :env_var => 'MAILER_SENDER_EMAIL'
 
 Settings.resolve!
