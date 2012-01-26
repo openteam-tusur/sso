2034cc994885412eb4ab08325cc5afbd06af3dfd
Dmitry Lihachev <lda@openteam.ru>
2012-01-26 13:20:48 +0700

    mailer.smtp_settings => mailer.smtp

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -10,12 +10,12 @@ Settings.define 'hoptoad.host',                               :env_var => 'HOPTO
 
 Settings.define 'mailer.sender_email',                        :env_var => 'SENDER_EMAIL', :required => true
 
-Settings.define 'mailer.smtp_settings.address',               :env_var => 'SMTP_ADDRESS', :required => true
-Settings.define 'mailer.smtp_settings.port',                  :env_var => 'SMTP_PORT'
-Settings.define 'mailer.smtp_settings.domain',                :env_var => 'SMTP_DOMAIN'
-Settings.define 'mailer.smtp_settings.user_name',             :env_var => 'SMTP_USER_NAME'
-Settings.define 'mailer.smtp_settings.password',              :env_var => 'SMTP_PASSWORD'
-Settings.define 'mailer.smtp_settings.authentication',        :env_var => 'SMTP_AUTHENTICATION'
-Settings.define 'mailer.smtp_settings.enable_starttls_auto',  :env_var => 'SMTP_ENABLE_STARTTLS_AUTO'
+Settings.define 'mailer.smtp.address',               :env_var => 'SMTP_ADDRESS', :required => true
+Settings.define 'mailer.smtp.port',                  :env_var => 'SMTP_PORT'
+Settings.define 'mailer.smtp.domain',                :env_var => 'SMTP_DOMAIN'
+Settings.define 'mailer.smtp.user_name',             :env_var => 'SMTP_USER_NAME'
+Settings.define 'mailer.smtp.password',              :env_var => 'SMTP_PASSWORD'
+Settings.define 'mailer.smtp.authentication',        :env_var => 'SMTP_AUTHENTICATION'
+Settings.define 'mailer.smtp.enable_starttls_auto',  :env_var => 'SMTP_ENABLE_STARTTLS_AUTO'
 
 Settings.resolve!
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.default_url_options = { :host => Settings[:host] }
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = Settings['mailer.smtp_settings']
+ActionMailer::Base.smtp_settings = Settings['mailer.smtp']
