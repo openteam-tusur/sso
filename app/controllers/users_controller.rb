0fa81988d3d6c2a8c78a23bf451f657b96c69acb
Dmitry Lihachev <lda@openteam.ru>
2012-11-16 09:05:40 +0700

    - gem has_searcher

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
-
-  inherit_resources
-  has_searcher
-
   def index
-    render :json => searcher.results.as_json(:only => :email, :methods => [:uid, :name])
+    render :json => User.search{ keywords params[:term] }.results.as_json(:only => :email, :methods => [:uid, :name])
   end
-
 end
