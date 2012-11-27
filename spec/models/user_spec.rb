ecd01ce9fe028e12652d999d5bb8fe07c0aa9ecc
Dmitry Lihachev <lda@openteam.ru>
2012-11-27 11:35:21 +0700

    внятное сообщение об ошибке в email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@
 require 'spec_helper'
 
 describe User do
-  shared_examples_for :field_with_only_cyrillic_letters do |field|
+  shared_examples_for :field_with_only_cyrillic_letters do |field, options|
     it { should allow_value("Иван").for(field) }
     it { should allow_value("Иван-Жак").for(field) }
     it { should allow_value("Иван-жак").for(field) }
     it { should allow_value("Иван Жак").for(field) }
     it { should allow_value("Иван жак").for(field) }
-    it { should_not allow_value("Иван_жак").for(field) }
+    it { should_not allow_value("Иван_жак").for(field).with_message(options[:error_message]) }
     it { should_not allow_value("иваН").for(field) }
     it { should_not allow_value("иван").for(field) }
     it { should_not allow_value("иван-Жак").for(field) }
@@ -22,23 +22,24 @@ describe User do
   describe '#first_name' do
     it { should validate_presence_of :first_name }
     it_should_behave_like :field_with_squish_normalizator, :first_name
-    it_should_behave_like :field_with_only_cyrillic_letters, :first_name
+    it_should_behave_like :field_with_only_cyrillic_letters, :first_name, :error_message => /должно состоять только из русских букв/
   end
 
   describe '#middle_name' do
     it { should_not validate_presence_of :middle_name }
     it_should_behave_like :field_with_squish_normalizator, :middle_name
-    it_should_behave_like :field_with_only_cyrillic_letters, :middle_name
+    it_should_behave_like :field_with_only_cyrillic_letters, :middle_name, :error_message => /должно состоять только из русских букв/
   end
 
   describe '#last_name' do
     it { should validate_presence_of :last_name }
     it_should_behave_like :field_with_squish_normalizator, :last_name
-    it_should_behave_like :field_with_only_cyrillic_letters, :last_name
+    it_should_behave_like :field_with_only_cyrillic_letters, :last_name, :error_message => /должна состоять только из русских букв/
   end
 
   describe '#email' do
     it { should validate_presence_of :email }
     it { should normalize_attribute :email }
+    it { should_not allow_value('completely wrong email').for(:email).with_message(/не похож на адрес электронной почты/) }
   end
 end
