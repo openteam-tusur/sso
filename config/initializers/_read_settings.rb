d170fec8a3f1976face58a7c770b4571250f51df
Manin Iliy <bkmzvf@gmail.com>
2013-09-12 13:41:36 +0700

    fix

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -1,6 +1,6 @@
 Settings.define 'app.url',                            :env_var => 'APP_URL',                    :required => true
 Settings.define 'mailer.sender_email',                :env_var => 'SENDER_EMAIL',               :required => true
-Settings.define 'mailer.smtp.address',                :env_var => 'SMTP_ADDRESS',               :required => true
+Settings.define 'mailer.smtp.address',                :env_var => 'SMTP_ADDRESS'
 Settings.define 'mailer.smtp.port',                   :env_var => 'SMTP_PORT'
 Settings.define 'mailer.smtp.domain',                 :env_var => 'SMTP_DOMAIN'
 Settings.define 'mailer.smtp.user_name',              :env_var => 'SMTP_USER_NAME'
