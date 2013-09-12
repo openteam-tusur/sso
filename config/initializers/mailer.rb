08a169c25e3b062d534dba9032500efbd341e8ee
Manin Iliy <bkmzvf@gmail.com>
2013-09-12 13:54:31 +0700

    read settings

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,4 +2,5 @@ settings = URI.parse(Settings['app.url'])
 
 ActionMailer::Base.default_url_options = { :host => settings.host, :scheme => settings.scheme, :port => settings.port }
 ActionMailer::Base.delivery_method = Settings['mailer.delivery_method'].to_sym
+ActionMailer::Base.postmark_settings = { :api_key => Settings['mailer.postmark.api_key'] } if Settings['mailer.delivery_method'] == 'postmark'
 ActionMailer::Base.smtp_settings = Settings['mailer.smtp'] if Settings['mailer.delivery_method'] == 'smtp'
