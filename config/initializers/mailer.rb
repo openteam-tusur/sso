7b4a333c5301d3cf5b3decd65a41bccd54c4abe3
Manin Iliy <bkmzvf@gmail.com>
2013-09-12 13:44:56 +0700

    fix

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,5 +1,5 @@
 settings = URI.parse(Settings['app.url'])
 
 ActionMailer::Base.default_url_options = { :host => settings.host, :scheme => settings.scheme, :port => settings.port }
-ActionMailer::Base.delivery_method = Settings['mailer.delivery_method']
+ActionMailer::Base.delivery_method = Settings['mailer.delivery_method'].to_sym
 ActionMailer::Base.smtp_settings = Settings['mailer.smtp'] if Settings['mailer.delivery_method'] == 'smtp'
