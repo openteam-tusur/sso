5e721fb30499f61c77ca97703e9ab0aa3c4647eb
Manin Iliy <bkmzvf@gmail.com>
2014-03-17 14:54:28 +0700

    remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,6 +76,10 @@ class User < ActiveRecord::Base
   def authenticatable_salt
     nil
   end
+
+  def remember_token
+    encrypted_password[0,29] if encrypted_password
+  end
 end
 # == Schema Information
 #
