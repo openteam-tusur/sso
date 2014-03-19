d2a3d5edcc9314d9db12d848b6578f8fbcc2af8f
tab <tukhfatullin@gmail.com>
2014-03-19 12:25:02 +0700

    Use Devise strong parameters for registration and profile update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_update_path_for(scope)
     session[:referrer] ? session[:referrer] : root_path
+  end
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:last_name, :first_name, :middle_name]
+    devise_parameter_sanitizer.for(:account_update) << [:last_name, :first_name, :middle_name]
   end
 
 end
