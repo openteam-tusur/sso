adf2b97658fcf2b50ed95a72695dbf1bcf4e0f38
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-06-09 16:00:48 +0700

    Revert "Hack after_sign_in method"
    This reverts commit 276197027bc29452069b6ecb41ae05ad73eba56f.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,6 @@
 class SessionsController < Devise::SessionsController
-  def after_sign_in_path_for(resource_or_scope)
-    redirect_uri
-  end
-
   def after_sign_out_path_for(resource_or_scope)
     request[:redirect_uri] || root_path
   end
-
-  private
-    def redirect_uri
-      return params['redirect_uri'] if params['redirect_uri']
-
-      url = if request.referer.try(:include?, 'redirect_uri=')
-        Rack::Utils.parse_nested_query(URI.parse(request.referer).query).try(:[], 'redirect_uri')
-      else
-        request.env['rack.session']['omniauth.origin']
-      end
-
-      url ? url : root_path
-    end
 end
 
