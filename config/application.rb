0c4106192d5b5fd2434645a637183820b1537311
Evgeny Lapin <lev@openteam.ru>
2016-05-30 15:45:02 +0700

    changed timezone

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(:default, Rails.env)
 
 module Sso
   class Application < Rails::Application
-    config.time_zone = 'Asia/Novosibirsk'
+    config.time_zone = 'Krasnoyarsk'
     config.i18n.default_locale = :ru
     I18n.enforce_available_locales = false
   end
